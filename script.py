dec30eea5fdde15f7dad7e0524d7d42d257c69d8
Manda ANDRIAMITANTSOA <mandriamitantsoa@gmail.com>
2025-10-31 11:32:29 +0300

    Refactor transaction filtering script: remove batch file, simplify function parameters, and enhance CSV transformation

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 import os
 import csv
 import datetime
+import pandas as pd
 
-def filter_invalid_transactions(input_dir, output_file, output_file_type_03):
+def filter_invalid_transactions(input_dir, output_file):
     """
     Iterate through all CSV files in the input directory and extract records where TYPE_TRANSACTION is missing.
     Save those records into the output file.
@@ -18,62 +19,50 @@ def filter_invalid_transactions(input_dir, output_file, output_file_type_03):
         writer_invalid_transactions.writerow(headers)  # Write headers to the output file
         
         for file_name in os.listdir(input_dir):
-            if file_name.endswith(".csv") and file_name not in [output_file_type_03, output_file]:
+            if file_name.endswith(".csv"):
                 file_path = os.path.join(input_dir, file_name)
                 temp_file_path = file_path + ".tmp"
                 
-                with open(file_path, mode='r+', encoding='utf-8') as in_csv, open(temp_file_path, mode='w', newline='', encoding='utf-8') as out_temp_fiile:
+                with open(file_path, mode='r+', encoding='utf-8') as in_csv, open(temp_file_path, mode='w', newline='', encoding='utf-8') as out_temp_file:
                     reader = csv.reader(in_csv, delimiter=';')
-                    writer_cursor_file = csv.writer(out_temp_fiile, delimiter=';')
+                    writer_cursor_file = csv.writer(out_temp_file, delimiter=';')
                     
                     headers_row = next(reader, None)
                     writer_cursor_file.writerow(headers_row)
                     
                     for row in reader:
-                        if len(row) >= 2 and not row[1].strip():
-                            if len(row) >= 8 and row[7].strip() == "01/01/2000":
-                                row[7] = ""
+                        if not row[1].strip():  # Check if TYPE_TRANSACTION is missing
                             writer_invalid_transactions.writerow(row)
-                        if len(row) >= 8 and (row[1].strip() == "01" or row[1].strip() == "02" or (row[1].strip() == "03" and row[7].strip() and row[7].strip() != "01/01/2000")):
-                            if (row[7].strip() == "01/01/2000"):
+                        else:
+                            if row[1].strip() in ["01", "03"]:
+                                row[7] = row[2]
+                            elif row[1].strip() == "02":
                                 row[7] = ""
                             writer_cursor_file.writerow(row)
-                os.replace(temp_file_path, file_path) 
-    
-def extract_type_03_missing_date_effet(input_dir, output_file, output_file_invalid):
-    """
-    Extract all records where TYPE_TRANSACTION is '03' but DATE_EFFET is missing and save them into another file.
-    """
-    headers = [
-        "REF_TRANSACTION_MVOLA", "TYPE_TRANSACTION", "DATE_TRANSACTION", "NUM_TELEPHONE_PAYEUR", 
-        "NOM_PRENOM_PAYEUR", "NUM_IDENTITE_PAYEUR", "NUM_CONTRAT", "DATE_EFFET", 
-        "MONTANT_TRANSACTION", "MONTANT_COMMISSION_MVOLA", "MONTANT_NET"
-    ]
-    
-    with open(output_file, mode='w', newline='', encoding='utf-8') as out_csv:
-        writer = csv.writer(out_csv, delimiter=';')
-        writer.writerow(headers)  # Write headers to the output file
-        
-        for file_name in os.listdir(input_dir):
-            if file_name.endswith(".csv") and file_name not in (output_file, output_file_invalid):
-                file_path = os.path.join(input_dir, file_name)
+                os.replace(temp_file_path, file_path)
                 
-                with open(file_path, mode='r', encoding='utf-8') as in_csv:
-                    reader = csv.reader(in_csv, delimiter=';')
-                    next(reader, None)  # Skip header row
-                    
-                    for row in reader:
-                        if len(row) >= 8 and row[1].strip() == "03" and (not row[7].strip() or row[7].strip() == "01/01/2000"):  # TYPE_TRANSACTION = 03 and DATE_EFFET is invalid
-                            writer.writerow(row)  # Write to output file
+def transform_excel_into_csv(input_dir):
+    """ Transform all excel files in the input directory into CSV files with ; separator """
+    for file_name in os.listdir(input_dir):
+        if file_name.endswith(".xlsx") or file_name.endswith(".xls"):
+            excel_file_path = os.path.join(input_dir, file_name)
+            base_name = os.path.splitext(file_name)[0]
+            
+            xls = pd.ExcelFile(excel_file_path)
+            for i, sheet_name in enumerate(xls.sheet_names, start=1):
+                df = xls.parse(sheet_name)
+                csv_file_name = f"{base_name}.csv"
+                csv_file_path = os.path.join(input_dir, csv_file_name)
+                
+                df.to_csv(csv_file_path, sep=';', index=False, encoding='utf-8')
+                print(f"Saved: {csv_file_path}")
 
 if __name__ == "__main__":
-    input_directory = os.getcwd() + "\\to_treat"  # Change this to the current directory
-    output_file_invalid = "erreurs_transaction_mvola.csv" # No type transaction specified
+    input_directory = os.getcwd() + "\\mvola\\to_treat"
     today = datetime.date.today()
-    output_file_type_03 = f"{today.strftime('%d-%m-%Y')}_type_03_missing_date_effet.csv" # Type 03 with no date effect
+    yesterday = today - datetime.timedelta(days=1)
+    output_file_invalid = f"mvola\\to_send_to_bo\\{yesterday.strftime('%Y-%m-%d')}_Transaction_Report_ARO_MVola_erreur.csv" # No type transaction specified
+    output_file_type_03 = f"mvola\\missing_date_effet\\{yesterday.strftime('%d-%m-%Y')}_type_03_missing_date_effet.csv" # Type 03 with no date effect
     
-    extract_type_03_missing_date_effet(input_directory, output_file_type_03, output_file_invalid)
-    filter_invalid_transactions(input_directory, output_file_invalid, output_file_type_03)
-    
-    print(f"Filtered records saved to {output_file_invalid}")
-    print(f"Type 03 transactions with missing DATE_EFFET saved to {output_file_type_03}")
+    filter_invalid_transactions(input_directory, output_file_invalid)
+    transform_excel_into_csv(input_directory)
